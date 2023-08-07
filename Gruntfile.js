83c11525a66c0a9745a7225746554e000bc540ee
XhmikosR <xhmikosr@gmail.com>
2023-08-07 11:18:00 +0300

    Switch to 2-space indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,256 +5,256 @@ var autoprefixer = require('autoprefixer');
 var combineDuplicateSelectors = require('postcss-combine-duplicated-selectors');
 
 module.exports = function(grunt) {
-    // Load any grunt plugins found in package.json.
-    require('load-grunt-tasks')(grunt);
-    require('@lodder/time-grunt')(grunt);
+  // Load any grunt plugins found in package.json.
+  require('load-grunt-tasks')(grunt);
+  require('@lodder/time-grunt')(grunt);
 
-    grunt.initConfig({
-        dirs: {
-            dest: '_site',
-            src: 'src',
-            tmp: '.tmp'
+  grunt.initConfig({
+    dirs: {
+      dest: '_site',
+      src: 'src',
+      tmp: '.tmp'
+    },
+
+    // Copy files that don't need compilation to _site/
+    copy: {
+      index: {
+        files: [{
+          dest: '<%= dirs.tmp %>/',
+          src: [
+            'index.html'
+          ],
+          filter: 'isFile',
+          expand: true,
+          cwd: '<%= dirs.src %>/'
+        }]
+      },
+      other: {
+        files: [{
+          dest: '<%= dirs.dest %>/',
+          src: [
+            '*',
+            '!index.html'
+          ],
+          filter: 'isFile',
+          dot: true,
+          expand: true,
+          cwd: '<%= dirs.src %>/'
+        }]
+      },
+      img: {
+        files: [{
+          dest: '<%= dirs.dest %>/',
+          src: 'img/**',
+          expand: true,
+          cwd: '<%= dirs.src %>/'
+        }]
+      }
+    },
+
+    sass: {
+      options: {
+        implementation: sass,
+        includePaths: ['./node_modules'],
+        outputStyle: 'compressed',
+        sourceMap: false
+      },
+      dist: {
+        src: '<%= dirs.src %>/css/main.scss',
+        dest: '<%= dirs.tmp %>/css/main.css'
+      }
+    },
+
+    concat: {
+      js: {
+        src: [
+          '<%= dirs.src %>/js/main.js',
+          '<%= dirs.src %>/js/google-analytics.js'
+        ],
+        dest: '<%= dirs.tmp %>/js/main.js'
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          combineDuplicateSelectors(),
+          autoprefixer()
+        ]
+      },
+      dist: {
+        src: '<%= sass.dist.dest %>'
+      }
+    },
+
+    purgecss: {
+      dist: {
+        options: {
+          content: [
+            '<%= dirs.tmp %>/**/*.html',
+            '<%= dirs.tmp %>/js/**/*.js'
+          ],
+          keyframes: true,
+          variables: false
         },
-
-        // Copy files that don't need compilation to _site/
-        copy: {
-            index: {
-                files: [{
-                    dest: '<%= dirs.tmp %>/',
-                    src: [
-                        'index.html'
-                    ],
-                    filter: 'isFile',
-                    expand: true,
-                    cwd: '<%= dirs.src %>/'
-                }]
-            },
-            other: {
-                files: [{
-                    dest: '<%= dirs.dest %>/',
-                    src: [
-                        '*',
-                        '!index.html'
-                    ],
-                    filter: 'isFile',
-                    dot: true,
-                    expand: true,
-                    cwd: '<%= dirs.src %>/'
-                }]
-            },
-            img: {
-                files: [{
-                    dest: '<%= dirs.dest %>/',
-                    src: 'img/**',
-                    expand: true,
-                    cwd: '<%= dirs.src %>/'
-                }]
-            }
-        },
-
-        sass: {
-            options: {
-                implementation: sass,
-                includePaths: ['./node_modules'],
-                outputStyle: 'compressed',
-                sourceMap: false
-            },
-            dist: {
-                src: '<%= dirs.src %>/css/main.scss',
-                dest: '<%= dirs.tmp %>/css/main.css'
-            }
-        },
-
-        concat: {
-            js: {
-                src: [
-                    '<%= dirs.src %>/js/main.js',
-                    '<%= dirs.src %>/js/google-analytics.js'
-                ],
-                dest: '<%= dirs.tmp %>/js/main.js'
-            }
-        },
-
-        postcss: {
-            options: {
-                processors: [
-                    combineDuplicateSelectors(),
-                    autoprefixer()
-                ]
-            },
-            dist: {
-                src: '<%= sass.dist.dest %>'
-            }
-        },
-
-        purgecss: {
-            dist: {
-                options: {
-                    content: [
-                        '<%= dirs.tmp %>/**/*.html',
-                        '<%= dirs.tmp %>/js/**/*.js'
-                    ],
-                    keyframes: true,
-                    variables: false
-                },
-                files: {
-                    '<%= sass.dist.dest %>': ['<%= sass.dist.dest %>']
-                }
-            }
-        },
-
-        staticinline: {
-            options: {
-                basepath: '<%= dirs.tmp %>/'
-            },
-            prod: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= dirs.tmp %>/',
-                    src: '*.html',
-                    dest: '<%= dirs.tmp %>/'
-                }]
-            },
-            dev: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= dirs.tmp %>/',
-                    src: '*.html',
-                    dest: '<%= dirs.dest %>/'
-                }]
-            }
-        },
-
-        htmlmin: {
-            dist: {
-                options: {
-                    collapseBooleanAttributes: true,
-                    collapseWhitespace: true,
-                    conservativeCollapse: false,
-                    decodeEntities: true,
-                    minifyCSS: {
-                        level: {
-                            1: {
-                                specialComments: 0
-                            },
-                            2: {
-                                all: false,
-                                mergeMedia: true,
-                                removeDuplicateMediaBlocks: true,
-                                removeEmpty: true
-                            }
-                        }
-                    },
-                    minifyJS: true,
-                    minifyURLs: false,
-                    processConditionalComments: true,
-                    removeAttributeQuotes: true,
-                    removeComments: true,
-                    removeOptionalTags: true,
-                    removeRedundantAttributes: true,
-                    removeScriptTypeAttributes: true,
-                    removeStyleLinkTypeAttributes: true,
-                    removeTagWhitespace: false,
-                    sortAttributes: true,
-                    sortClassName: true
-                },
-                expand: true,
-                cwd: '<%= dirs.tmp %>',
-                dest: '<%= dirs.dest %>',
-                src: [
-                    '*.html',
-                    '!404.html'
-                ]
-            }
-        },
-
-        eslint: {
-            dist: {
-                src: [
-                    'Gruntfile.js',
-                    '<%= dirs.src %>/js/*.js'
-                ]
-            }
-        },
-
-        htmllint: {
-            src: [
-                '<%= dirs.dest %>/*.html',
-                '!<%= dirs.dest %>/google*.html'
-            ]
-        },
-
-        connect: {
-            options: {
-                hostname: 'localhost',
-                livereload: 35729,
-                port: 8001
-            },
-            livereload: {
-                options: {
-                    base: '<%= dirs.dest %>/',
-                    open: true  // Automatically open the webpage in the default browser
-                }
-            }
-        },
-
-        watch: {
-            options: {
-                livereload: '<%= connect.options.livereload %>'
-            },
-            dev: {
-                files: ['<%= dirs.src %>/**', 'Gruntfile.js'],
-                tasks: 'build:dev'
-            },
-            build: {
-                files: ['<%= dirs.src %>/**', 'Gruntfile.js'],
-                tasks: 'build'
-            }
-        },
-
-        clean: {
-            dist: [
-                '<%= dirs.tmp %>/',
-                '<%= dirs.dest %>/'
-            ]
+        files: {
+          '<%= sass.dist.dest %>': ['<%= sass.dist.dest %>']
         }
-    });
+      }
+    },
 
-    grunt.registerTask('build:dev', [
-        'clean',
-        'copy',
-        'concat',
-        'sass',
-        'postcss',
-        'staticinline:dev'
-    ]);
+    staticinline: {
+      options: {
+        basepath: '<%= dirs.tmp %>/'
+      },
+      prod: {
+        files: [{
+          expand: true,
+          cwd: '<%= dirs.tmp %>/',
+          src: '*.html',
+          dest: '<%= dirs.tmp %>/'
+        }]
+      },
+      dev: {
+        files: [{
+          expand: true,
+          cwd: '<%= dirs.tmp %>/',
+          src: '*.html',
+          dest: '<%= dirs.dest %>/'
+        }]
+      }
+    },
 
-    grunt.registerTask('build', [
-        'clean',
-        'copy',
-        'concat',
-        'sass',
-        'postcss',
-        'purgecss',
-        'staticinline:prod',
-        'htmlmin'
-    ]);
+    htmlmin: {
+      dist: {
+        options: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          conservativeCollapse: false,
+          decodeEntities: true,
+          minifyCSS: {
+            level: {
+              1: {
+                specialComments: 0
+              },
+              2: {
+                all: false,
+                mergeMedia: true,
+                removeDuplicateMediaBlocks: true,
+                removeEmpty: true
+              }
+            }
+          },
+          minifyJS: true,
+          minifyURLs: false,
+          processConditionalComments: true,
+          removeAttributeQuotes: true,
+          removeComments: true,
+          removeOptionalTags: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          removeTagWhitespace: false,
+          sortAttributes: true,
+          sortClassName: true
+        },
+        expand: true,
+        cwd: '<%= dirs.tmp %>',
+        dest: '<%= dirs.dest %>',
+        src: [
+          '*.html',
+          '!404.html'
+        ]
+      }
+    },
 
-    grunt.registerTask('test', [
-        'build',
-        'eslint',
-        'htmllint'
-    ]);
+    eslint: {
+      dist: {
+        src: [
+          'Gruntfile.js',
+          '<%= dirs.src %>/js/*.js'
+        ]
+      }
+    },
 
-    grunt.registerTask('default', [
-        'build:dev',
-        'connect',
-        'watch:dev'
-    ]);
+    htmllint: {
+      src: [
+        '<%= dirs.dest %>/*.html',
+        '!<%= dirs.dest %>/google*.html'
+      ]
+    },
 
-    grunt.registerTask('server', [
-        'build',
-        'connect',
-        'watch:build'
-    ]);
+    connect: {
+      options: {
+        hostname: 'localhost',
+        livereload: 35729,
+        port: 8001
+      },
+      livereload: {
+        options: {
+          base: '<%= dirs.dest %>/',
+          open: true  // Automatically open the webpage in the default browser
+        }
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: '<%= connect.options.livereload %>'
+      },
+      dev: {
+        files: ['<%= dirs.src %>/**', 'Gruntfile.js'],
+        tasks: 'build:dev'
+      },
+      build: {
+        files: ['<%= dirs.src %>/**', 'Gruntfile.js'],
+        tasks: 'build'
+      }
+    },
+
+    clean: {
+      dist: [
+        '<%= dirs.tmp %>/',
+        '<%= dirs.dest %>/'
+      ]
+    }
+  });
+
+  grunt.registerTask('build:dev', [
+    'clean',
+    'copy',
+    'concat',
+    'sass',
+    'postcss',
+    'staticinline:dev'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean',
+    'copy',
+    'concat',
+    'sass',
+    'postcss',
+    'purgecss',
+    'staticinline:prod',
+    'htmlmin'
+  ]);
+
+  grunt.registerTask('test', [
+    'build',
+    'eslint',
+    'htmllint'
+  ]);
+
+  grunt.registerTask('default', [
+    'build:dev',
+    'connect',
+    'watch:dev'
+  ]);
+
+  grunt.registerTask('server', [
+    'build',
+    'connect',
+    'watch:build'
+  ]);
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,116 +1,116 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    function getElement(selector) {
-        return document.querySelector(selector);
+  function getElement(selector) {
+    return document.querySelector(selector);
+  }
+
+  function addInvalidClass(element) {
+    return element.classList.add('is-invalid');
+  }
+
+  function removeErrorClass(element) {
+    return element.classList.remove('is-invalid');
+  }
+
+  function setBooleanAttribute(element, attribute) {
+    return element.setAttribute(attribute, '');
+  }
+
+  function removeAttribute(element, attribute) {
+    return element.removeAttribute(attribute);
+  }
+
+  var totalCost = getElement('#totalCost');
+  var vatRate = getElement('#vatRate');
+  var totalVat = getElement('#totalVat');
+  var totalNetCost = getElement('#totalNetCost');
+  var calcBtn = getElement('#calcBtn');
+  var resetBtn = getElement('#resetBtn');
+  var calcForm = getElement('#calcForm');
+
+  function calculateVAT() {
+    if (totalCost.hasAttribute('disabled')) {
+      totalCost.value = (Number(totalNetCost.value) + (Number(vatRate.value / 100) * totalNetCost.value)).toFixed(2);
+      totalVat.value = (Number(totalCost.value) - Number(totalNetCost.value)).toFixed(2);
+
+      removeAttribute(totalVat, 'disabled');
+      removeAttribute(totalCost, 'disabled');
+      setBooleanAttribute(totalCost, 'readonly');
+    } else {
+      totalNetCost.value = (Number(totalCost.value) / (1 + Number(vatRate.value / 100))).toFixed(2);
+      totalVat.value = (Number(totalCost.value) - Number(totalNetCost.value)).toFixed(2);
+
+      removeAttribute(totalVat, 'disabled');
+      removeAttribute(totalNetCost, 'disabled');
+      setBooleanAttribute(totalNetCost, 'readonly');
     }
 
-    function addInvalidClass(element) {
-        return element.classList.add('is-invalid');
+    setBooleanAttribute(calcBtn, 'disabled');
+  }
+
+  function resetCalculator() {
+    calcForm.reset();
+
+    calcForm.classList.remove('was-validated');
+    removeErrorClass(totalCost);
+    removeErrorClass(totalNetCost);
+    removeErrorClass(vatRate);
+
+    removeAttribute(totalCost, 'readonly');
+    removeAttribute(totalNetCost, 'readonly');
+    removeAttribute(totalCost, 'disabled');
+    removeAttribute(totalNetCost, 'disabled');
+
+    removeAttribute(calcBtn, 'disabled');
+  }
+
+  totalCost.addEventListener('input', function() {
+    totalCost.classList.add('was-validated');
+
+    if (totalCost.validity.valid) {
+      removeErrorClass(totalCost);
+
+      setBooleanAttribute(totalNetCost, 'disabled');
+      setBooleanAttribute(totalNetCost, 'readonly');
+      removeAttribute(calcBtn, 'disabled');
+    } else {
+      addInvalidClass(totalCost);
+      setBooleanAttribute(calcBtn, 'disabled');
     }
+  });
 
-    function removeErrorClass(element) {
-        return element.classList.remove('is-invalid');
+  totalNetCost.addEventListener('input', function() {
+    totalNetCost.classList.add('was-validated');
+
+    if (totalNetCost.validity.valid) {
+      removeErrorClass(totalNetCost);
+
+      setBooleanAttribute(totalCost, 'disabled');
+      setBooleanAttribute(totalCost, 'readonly');
+      removeAttribute(calcBtn, 'disabled');
+    } else {
+      addInvalidClass(totalNetCost);
+      setBooleanAttribute(calcBtn, 'disabled');
     }
+  });
 
-    function setBooleanAttribute(element, attribute) {
-        return element.setAttribute(attribute, '');
+  vatRate.addEventListener('input', function() {
+    vatRate.classList.add('was-validated');
+
+    if (vatRate.validity.valid) {
+      removeErrorClass(vatRate);
+      removeAttribute(calcBtn, 'disabled');
+    } else {
+      addInvalidClass(vatRate);
+      setBooleanAttribute(calcBtn, 'disabled');
     }
+  });
 
-    function removeAttribute(element, attribute) {
-        return element.removeAttribute(attribute);
-    }
+  calcForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+  });
 
-    var totalCost = getElement('#totalCost');
-    var vatRate = getElement('#vatRate');
-    var totalVat = getElement('#totalVat');
-    var totalNetCost = getElement('#totalNetCost');
-    var calcBtn = getElement('#calcBtn');
-    var resetBtn = getElement('#resetBtn');
-    var calcForm = getElement('#calcForm');
-
-    function calculateVAT() {
-        if (totalCost.hasAttribute('disabled')) {
-            totalCost.value = (Number(totalNetCost.value) + (Number(vatRate.value / 100) * totalNetCost.value)).toFixed(2);
-            totalVat.value = (Number(totalCost.value) - Number(totalNetCost.value)).toFixed(2);
-
-            removeAttribute(totalVat, 'disabled');
-            removeAttribute(totalCost, 'disabled');
-            setBooleanAttribute(totalCost, 'readonly');
-        } else {
-            totalNetCost.value = (Number(totalCost.value) / (1 + Number(vatRate.value / 100))).toFixed(2);
-            totalVat.value = (Number(totalCost.value) - Number(totalNetCost.value)).toFixed(2);
-
-            removeAttribute(totalVat, 'disabled');
-            removeAttribute(totalNetCost, 'disabled');
-            setBooleanAttribute(totalNetCost, 'readonly');
-        }
-
-        setBooleanAttribute(calcBtn, 'disabled');
-    }
-
-    function resetCalculator() {
-        calcForm.reset();
-
-        calcForm.classList.remove('was-validated');
-        removeErrorClass(totalCost);
-        removeErrorClass(totalNetCost);
-        removeErrorClass(vatRate);
-
-        removeAttribute(totalCost, 'readonly');
-        removeAttribute(totalNetCost, 'readonly');
-        removeAttribute(totalCost, 'disabled');
-        removeAttribute(totalNetCost, 'disabled');
-
-        removeAttribute(calcBtn, 'disabled');
-    }
-
-    totalCost.addEventListener('input', function() {
-        totalCost.classList.add('was-validated');
-
-        if (totalCost.validity.valid) {
-            removeErrorClass(totalCost);
-
-            setBooleanAttribute(totalNetCost, 'disabled');
-            setBooleanAttribute(totalNetCost, 'readonly');
-            removeAttribute(calcBtn, 'disabled');
-        } else {
-            addInvalidClass(totalCost);
-            setBooleanAttribute(calcBtn, 'disabled');
-        }
-    });
-
-    totalNetCost.addEventListener('input', function() {
-        totalNetCost.classList.add('was-validated');
-
-        if (totalNetCost.validity.valid) {
-            removeErrorClass(totalNetCost);
-
-            setBooleanAttribute(totalCost, 'disabled');
-            setBooleanAttribute(totalCost, 'readonly');
-            removeAttribute(calcBtn, 'disabled');
-        } else {
-            addInvalidClass(totalNetCost);
-            setBooleanAttribute(calcBtn, 'disabled');
-        }
-    });
-
-    vatRate.addEventListener('input', function() {
-        vatRate.classList.add('was-validated');
-
-        if (vatRate.validity.valid) {
-            removeErrorClass(vatRate);
-            removeAttribute(calcBtn, 'disabled');
-        } else {
-            addInvalidClass(vatRate);
-            setBooleanAttribute(calcBtn, 'disabled');
-        }
-    });
-
-    calcForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-    });
-
-    calcBtn.addEventListener('click', calculateVAT);
-    resetBtn.addEventListener('click', resetCalculator);
+  calcBtn.addEventListener('click', calculateVAT);
+  resetBtn.addEventListener('click', resetCalculator);
 })();
